4d7378ed6f6bb46df4673c1eddf0f6114748d203
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-10 23:14:35 +0530

    Added Firebase Authentication for registering new users.

--- app/src/main/java/com/example/messanger/Login.kt
+++ app/src/main/java/com/example/messanger/Login.kt
@@ -13,6 +13,7 @@ class Login : AppCompatActivity() {
         Registration_page.setOnClickListener {
             val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/messanger/MainActivity.kt
+++ app/src/main/java/com/example/messanger/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.messanger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,37 @@ class MainActivity : AppCompatActivity() {
         already_accunt.setOnClickListener {
             val intent = Intent(this,Login::class.java)
             startActivity(intent)
+            finish()
+        }
+        register_btn.setOnClickListener {
+            performregister()
+        }
+    }
+    private fun performregister(){
+        val username= username_registration.text.toString()
+        val email = email_registration.text.toString()
+        val password= registration_password.text.toString()
+        val etusername:EditText = findViewById(R.id.username_registration)
+        val etemail:EditText=findViewById(R.id.email_registration)
+        val etpassword:EditText= findViewById(R.id.registration_password)
+        if (email.isEmpty()){
+            etemail.error="Email Required"
+            etemail.setBackgroundResource(R.drawable.input_field_error)
+        }else if (password.isEmpty()){
+            etpassword.error="Required Password"
+            etpassword.setBackgroundResource(R.drawable.input_field_error)
+        }else if (username.isEmpty()){
+            etusername.error="Required Username"
+            etusername.setBackgroundResource(R.drawable.input_field_error)
+        }else{
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("Register","Account created successfully.")
+                    Toast.makeText(this,"Account created successfully",Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    Toast.makeText(this,"Failed to create Account ${it.message}",Toast.LENGTH_SHORT).show()
+                }
         }
     }
 

34694c7fbdfc40ca15a4d3750c48e989025beb23
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-12 16:30:19 +0530

    Created menu resource directory.

--- app/src/main/java/com/example/messanger/LatestMessages.kt
+++ app/src/main/java/com/example/messanger/LatestMessages.kt
@@ -1,12 +1,31 @@
 package com.example.messanger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessages : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
+        
+      verifyuserloggedin()
+        }
+
+    private fun verifyuserloggedin(){
+    val uid = FirebaseAuth.getInstance().uid
+    if (uid== null){
+        val intent = Intent(this,MainActivity::class.java)
+        intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nac_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
+

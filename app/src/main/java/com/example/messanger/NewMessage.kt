f16ebf9a45924036a8d854f800fd50e00c544cfb
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-12 17:34:07 +0530

    Created user selecting row resource file.

--- app/src/main/java/com/example/messanger/NewMessage.kt
+++ app/src/main/java/com/example/messanger/NewMessage.kt
@@ -2,6 +2,9 @@ package com.example.messanger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessage : AppCompatActivity() {
@@ -10,6 +13,17 @@ class NewMessage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
         supportActionBar?.title="Select User"
-        recycler_view_users.adapter
+
+        val adaptor = GroupAdapter<ViewHolder>()
+        recycler_view_users.adapter=adaptor
+
+    }
+}
+class Useritem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+    override fun getLayout(): Int {
+        return R.layout.
     }
 }

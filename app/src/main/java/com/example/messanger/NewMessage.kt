78818869281958fdfd2ac6e864e07ddcea22d8de
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-12 17:24:13 +0530

    Added recycler view in select user activity.

--- app/src/main/java/com/example/messanger/NewMessage.kt
+++ app/src/main/java/com/example/messanger/NewMessage.kt
@@ -2,11 +2,14 @@ package com.example.messanger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+        supportActionBar?.title="Select User"
+        recycler_view_users.adapter
     }
 }

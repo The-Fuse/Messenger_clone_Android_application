a2f5c0458be59439930aa5db46e02e8ee8292945
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-12 16:35:55 +0530

    Added Signout action button.

--- app/src/main/java/com/example/messanger/LatestMessages.kt
+++ app/src/main/java/com/example/messanger/LatestMessages.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessages : AppCompatActivity() {
@@ -21,6 +22,21 @@ class LatestMessages : AppCompatActivity() {
         val intent = Intent(this,MainActivity::class.java)
         intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)}
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         when(item?.itemId){
+             R.id.menu_new_message ->{
+
+             }
+             R.id.menu_sign_out ->{
+                 FirebaseAuth.getInstance().signOut()
+                 val intent = Intent(this,MainActivity::class.java)
+                 intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                 startActivity(intent)
+             }
+         }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
